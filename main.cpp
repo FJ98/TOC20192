a42ae43d44f24f7bc2d988196606f3fb21aceac9
FJ98 <felix.solano@utec.edu.pe>
2019-10-31 13:26:36 -0500

    variables del problema

--- main.cpp
+++ main.cpp
@@ -3,5 +3,17 @@ using namespace std;
 
 int main() {
     cout << "Hello World!" << endl;
+
+    std::vector<std::string> Z1 = {"ein", "zwei", "drei", "vier", "funf", "sechs", "sieben", "acht", "neun"};
+    std::vector<std::string> Z2 = {"zehn", "elf", "zwolf" };
+    std::vector<std::string> Z3 = {"drei", "vier", "funf", "sech", "sieb", "acht", "neun" };
+    std::vector<std::string> Z4 = {"zwan", "vier", "funf", "sech", "sieb", "acht", "neun" };
+    std::vector<std::string> Z5 = {"hundert"};
+    std::vector<std::string> Z6 = {"tausend"};
+    std::vector<std::string> drei = {"drei"};
+    std::vector<std::string> zehn = {"zehn"};
+    std::vector<std::string> zig = {"zig"};
+    std::vector<std::string> ssig = {"ssig"};
+
     return 0;
 }
